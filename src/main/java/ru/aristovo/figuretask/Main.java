ace627190bb0ee420d4a94e77d409804f43c6019
Oleg Pushkarev <tyler98765@yandex.ru>
2021-01-24 19:41:03 +0300

    Обновлен класс Main, создали три фигуры и нашли их площадь.

--- src/main/java/ru/aristovo/figuretask/Main.java
+++ src/main/java/ru/aristovo/figuretask/Main.java
@@ -1,10 +1,21 @@
 package ru.aristovo.figuretask;
 
+import ru.aristovo.figuretask.figure.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Создаем треугольник, квадрат и ромб, и находим их площади");
 
+        Figure triangle = new Triangle(5.4,2.6);
+        System.out.println("Площадь треугольника = " + triangle.calcArea());
+
+        Figure square = new Square(3.3);
+        System.out.println("Площадь квадрата = " + square.calcArea());
+
+        Figure rhombus = new Rhombus(1.5,6.1);
+        System.out.println("Площадь ромба = " + rhombus.calcArea());
 
     }
 }
